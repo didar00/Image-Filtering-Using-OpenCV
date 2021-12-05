c699a363607544f214c4cc549d99062ed5f1e691
didar00 <didartufekcigil@gmail.com>
2021-12-05 17:39:47 +0300

    mean filter was added

--- main.py
+++ main.py
@@ -9,45 +9,15 @@ Kuwahara filter implementation using HSV color space
 """
 
 # Read the image - Notice that OpenCV reads the images as BRG instead of RGB
-img = cv2.imread('image.jpg')
-print(img.shape)
-""" plt.imshow(img)
-plt.show() """
-""" img = cv2.resize(img, (img.shape[:2]/2))
-plt.imshow(img)
-plt.show() """
+img = cv2.imread('dog.jpg')
 
-# Convert the BRG image to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-print(img)
-print()
-print()
 
-img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-print(img)
-print()
-print()
-
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-print(img)
-print()
-print()
-
-""" plt.imshow(img)
-plt.show()
- """
-""" # Convert the RGB image to HSV
-img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) """
-""" plt.imshow(img)
-plt.show()
-
-print(img.shape)
-print(img[0]) """
 
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+""" img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(img, cmap="gray", vmin=0, vmax=255)
-plt.show()
+plt.show() """
 
 
 
@@ -71,7 +41,7 @@ def kuwahara_filter(image, win_size):
             # find the top left corner of the filter on image
             tl_x = x - int(win_size/2)
             tl_y = y - int(win_size/2)
-            #print(clamp_y(tl_y, tl_y + quadrant_size))
+
             # find the quarters of the image
             q1 = [clamp_y(tl_y, tl_y + quadrant_size), clamp_x(tl_x, tl_x + quadrant_size)]
             q2 = [clamp_y(tl_y, tl_y + quadrant_size), clamp_x(tl_x, tl_x + quadrant_size)]
@@ -87,14 +57,10 @@ def kuwahara_filter(image, win_size):
             min_std = np.argmin([std_1, std_2, std_3, std_4])
 
             selected_quad = quads[min_std]
-            #print(image[selected_quad[0][0]:selected_quad[0][1], selected_quad[1][0]:selected_quad[1][1],2])
-            #if image[selected_quad[0][0]:selected_quad[0][1], selected_quad[1][0]:selected_quad[1][1],2].size != np.array([]):
-            #print(selected_quad[0][0],selected_quad[0][1], "   ", selected_quad[1][0],selected_quad[1][1])
-            if selected_quad[0][0] != selected_quad[0][1] and selected_quad[1][0] != selected_quad[1][1]:
+           if selected_quad[0][0] != selected_quad[0][1] and selected_quad[1][0] != selected_quad[1][1]:
                 new_pixel_val = image[selected_quad[0][0]:selected_quad[0][1], selected_quad[1][0]:selected_quad[1][1],2].mean()
                 img[y,x,2] = new_pixel_val
-            #print(x,y)
-            
+
 
     return img
 
@@ -104,7 +70,9 @@ def gaussian_formula(x,y,sigma):
 
 
 def gaussian_filter(image, filter_size, sigma=1):
+    print(image.shape)
     from itertools import product
+    img = image[:,:,2]
     center = filter_size//2
     x,y = np.mgrid[0-center:filter_size-center, 0-center:filter_size-center]
     filter_ = gaussian_formula(x,y,sigma)
@@ -120,30 +88,59 @@ def gaussian_filter(image, filter_size, sigma=1):
 
     row = 0
     for i,j in product(range(new_img_height), range(new_img_width)):
-        new_image[row,:] = np.ravel(image[i:i+filter_size, j:j+filter_size])
+        new_image[row,:] = np.ravel(img[i:i+filter_size, j:j+filter_size])
         row += 1
 
     filter_ = np.ravel(filter_)
     filtered_image = np.dot(new_image, filter_).reshape(new_img_height, new_img_width).astype(np.uint8)
+    image[center:new_img_height+center,center:new_img_width+center,2] = filtered_image
 
 
-    print(filter_)
-    print(filtered_image)
+    return image
+
+
+
+def mean_filter(image, filter_size):
+    from itertools import product
+    # convert the image into windows
+    # to average them pixel by pixel
+    center = filter_size//2
+    # new image's dimensions since
+    # the border values are discarded
+    new_img_height = image.shape[0] - filter_size + 1
+    new_img_width = image.shape[1] - filter_size + 1  
+
+    # store each window in numpy array to
+    # average them at once to obtain the
+    # new pixel values
+    # preserve color channels
+    windows = np.empty((new_img_height*new_img_width, filter_size**2, 3))
+
+
+    row = 0
+    # store each window in "windows" as the filter
+    # traces the image
+    for i,j in product(range(new_img_height), range(new_img_width)):
+        # simply, all pixels under the filter side to side
+        # with their RGB channels
+        windows[row,:,:] = np.ravel(image[i:i+filter_size, j:j+filter_size]).reshape(-1,3)
+        row += 1
+
+    filtered_image =np.empty((new_img_height*new_img_width,3))
+
+    # calculate the mean for RGB values seperately for each window
+    for r in range(new_img_height*new_img_width):
+        filtered_image[r,:] = windows[r,:,:].mean(axis=0)
+    
+    # after calculating the means for each channel and row
+    # in windows, reshape the array to obtain resultant
+    # image with RGB channels
+    filtered_image = filtered_image.reshape(new_img_height, new_img_width, 3).astype(np.uint8)
 
     return filtered_image
 
 
 
-""" filtered_img = kuwahara_filter(img, 9)
-plt.imshow(filtered_img)
-plt.show()
-filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_HSV2RGB)
-plt.imshow(filtered_img)
-plt.show() """
-
-filtered_img = gaussian_filter(img, 9)
-filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_GRAY2RGB)
-print(filtered_img)
-#plt.imshow(filtered_img, cmap="gray", vmin=0, vmax=255)
-plt.imshow(filtered_img)
+filtered_image = mean_filter(img,7)
+plt.imshow(filtered_image)
 plt.show()
